8379ab95fd74dca1484035013323ee57c89d2c8d
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2022-10-03 17:15:29 +0200

    Modificacio UFs

--- APIFirst/src/main/java/net/jaumebalmes/m12/objectes/UFs.java
+++ APIFirst/src/main/java/net/jaumebalmes/m12/objectes/UFs.java
@@ -1,5 +1,32 @@
 package net.jaumebalmes.m12.objectes;
 
 public class UFs {
+	private String nom;
+	private int posicio;
+	
+	public UFs(String nom, int posicio) {
+		super();
+		this.nom = nom;
+		this.posicio = posicio;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPosicio() {
+		return posicio;
+	}
+
+	public void setPosicio(int posicio) {
+		this.posicio = posicio;
+	}
+	
+	
+	
 }
+
