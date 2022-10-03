1a0a9ed6ae7cf60d75eff644962b4a8432840da0
ReymonVega <rvega@jaumebalmes.net>
2022-10-03 17:29:04 +0200

    Clase Objetos Echa

--- APIFirst/src/main/java/net/jaumebalmes/m12/objectes/Moduls.java
+++ APIFirst/src/main/java/net/jaumebalmes/m12/objectes/Moduls.java
@@ -1,5 +1,43 @@
 package net.jaumebalmes.m12.objectes;
 
+
 public class Moduls {
+    private String nombreDelModul;
+    private int duracion;
+    private String ufsModuls ;
+
+    public String getNombreDelModul() {
+        return nombreDelModul;
+    }
+
+    public void setNombreDelModul(String nombreDelModul) {
+        this.nombreDelModul = nombreDelModul;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getUfsModuls() {
+        return ufsModuls;
+    }
+
+    public void setUfsModuls(String ufsModuls) {
+        this.ufsModuls = ufsModuls;
+    }
+
+    //Constructor
+
+    public Moduls(String nombreDelModul, int duracion, String ufsModuls) {
+        this.nombreDelModul = nombreDelModul;
+        this.duracion = duracion;
+        this.ufsModuls = ufsModuls;
+    }
+    
+    
 
 }
