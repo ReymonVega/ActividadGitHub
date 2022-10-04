2864de6a1ea8321316d26c3e4fdec3bfbbb32c19
sebastian11223 <mcasco@jaumebalmes.net>
2022-10-04 16:18:57 +0200

    Hehco el primer GetMapping de la clase Alumne

--- APIFirst/src/main/java/net/jaumebalmes/m12/controller/Controller.java
+++ APIFirst/src/main/java/net/jaumebalmes/m12/controller/Controller.java
@@ -1,5 +1,19 @@
 package net.jaumebalmes.m12.controller;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.jaumebalmes.m12.objectes.Alumne;
+
+@RestController
 public class Controller {
 	
+	@GetMapping("alumnes")
+	public Alumne getAlumne() {
+		Alumne Alumnes = new Alumne("Javier","Corona Alba",LocalDate.of(2000,Month.MAY, 17),"Jaume Balmes",2);
+		return Alumnes;
+	}
 }
