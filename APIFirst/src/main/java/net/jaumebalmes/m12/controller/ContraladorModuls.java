dcf864ef48a4cc0e57afb8ec5b446633e110135b
ReymonVega <rvega@jaumebalmes.net>
2022-10-06 16:33:11 +0200

    New Controller and Class Moduls

--- APIFirst/src/main/java/net/jaumebalmes/m12/controller/ContraladorModuls.java
+++ APIFirst/src/main/java/net/jaumebalmes/m12/controller/ContraladorModuls.java
@@ -1,31 +1,15 @@
 package net.jaumebalmes.m12.controller;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-import net.jaumebalmes.m12.objectes.Alumne;
 import net.jaumebalmes.m12.objectes.Moduls;
 
-@RestController
-public class Controller {
+public class ContraladorModuls {
 	
-	
-	//PartSebastianCasco
-	@GetMapping("alumnes")
-
-	public Alumne getAlumne() {
-		Alumne Alumnes = new Alumne("Javier","Corona Alba",LocalDate.of(2000,Month.MAY, 17),"Jaume Balmes",2);
-		return Alumnes;
-	}
-	
-	
-	//Part ReymonVega
 	@GetMapping("modulos")
 	public List<Moduls> getModuls(){
 		
@@ -51,4 +35,6 @@ public class Controller {
 		
 		return listaModuls.get(nModuls -1);
 	}
+
+
 }
