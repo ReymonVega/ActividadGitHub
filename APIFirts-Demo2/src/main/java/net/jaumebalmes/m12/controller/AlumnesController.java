71d588299dc8c282c6688435ad3863453d981782
Reymon Vega <rvega@jaumebalmes.net>
2022-10-24 19:23:49 +0200

    Función @DeleteMapping (resuelta)

--- APIFirts-Demo2/src/main/java/net/jaumebalmes/m12/controller/AlumnesController.java
+++ APIFirts-Demo2/src/main/java/net/jaumebalmes/m12/controller/AlumnesController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +43,13 @@ public class AlumnesController {
 		
 		return client;
 	}
+	
+	
+	@DeleteMapping("alumnes/{id}")
+    public ResponseEntity<Alumne>  deleteAlumne(@PathVariable Long id) {
+        alumeRepo.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 	
 	//@DeleteMapping("clients/{id}")
 	
