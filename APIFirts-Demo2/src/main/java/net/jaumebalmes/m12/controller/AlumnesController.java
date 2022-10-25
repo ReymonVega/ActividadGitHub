2a0afaa08fcd54b049ae5473370f8f4721dbc648
sebastian11223 <mcasco@jaumebalmes.net>
2022-10-25 17:06:30 +0200

    método PatchMapping por HACER

--- APIFirts-Demo2/src/main/java/net/jaumebalmes/m12/controller/AlumnesController.java
+++ APIFirts-Demo2/src/main/java/net/jaumebalmes/m12/controller/AlumnesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -50,6 +51,13 @@ public class AlumnesController {
         alumeRepo.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+	
+	//@PatchMapping("alumnes/{id}")
+	//public ResponseEntity<Alumne> patchAlumne(@PathVariable Long id){
+		//alumeRepo.patchById(id);
+		//alumeRepo.save(id);
+		//return ResponseEntity.noContent().build();
+	//}
 	
 	//@DeleteMapping("clients/{id}")
 	
